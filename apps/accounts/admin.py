109e83ab4a838188ace8784740fb472ac9b67b64
alireza <alirezashateri7@gmail.com>
2020-08-23 16:22:20 +0430

    Add custom User model for best practice purposes

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BuiltInUserAdmin
+
+from .models import User
+
 
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(BuiltInUserAdmin):
+    pass
