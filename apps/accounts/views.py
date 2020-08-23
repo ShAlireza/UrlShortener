30197da52ef8c316249b0cc12e8c6e9c9dc84703
alireza <alirezashateri7@gmail.com>
2020-08-23 17:30:10 +0430

    Add signup api view

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.utils.translation import gettext_lazy as _
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+
+from .serializers import UserSerializer
+
+LoginAPIView = ObtainAuthToken
+
+
+class SignUpAPIView(GenericAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.save()
+        token = Token.objects.create(user=user)
+        return Response(data={'detail': _('Welcome!'), 'token': token},
+                        status=status.HTTP_201_CREATED)
