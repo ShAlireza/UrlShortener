9924e5add4ec65b6d5a0ddf1f1c2bed56e1f00a3
alireza <alirezashateri7@gmail.com>
2020-08-24 00:59:24 +0430

    Add ShortenedUrl model

--- apps/shortener/models.py
+++ apps/shortener/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from rest_framework.exceptions import ValidationError
+
+
+class ShortenedUrl(models.Model):
+    source = models.CharField(max_length=512)
+    destination = models.CharField(max_length=512, blank=True, null=True,
+                                   unique=True)
+    hits = models.IntegerField(default=0)
+
+    def __str__(self):
+        return '%s %s' % (self.source, self.destination)
+
+    def clean(self):
+        if not (self.destination.startswith("http") or
+                self.destination.startswith("https")):
+            raise ValidationError('URL should start with "http" or "https"')
