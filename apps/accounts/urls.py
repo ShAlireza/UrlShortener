c36dfb00c1756e7e57f2e6f14bd81299dd10f9c7
alireza <alirezashateri7@gmail.com>
2020-08-23 17:35:26 +0430

    Add login api and accounts app urls

---
+++ apps/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('login', view=views.LoginAPIView.as_view(), name='login'),
+    path('signup', view=views.SignUpAPIView.as_view(), name='signup'),
+]
