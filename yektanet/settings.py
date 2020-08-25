18dda8956889ed96a1eb2aaf5666efbb155f8486
alireza <alirezashateri7@gmail.com>
2020-08-25 18:03:48 +0430

    Add redis cache

--- yektanet/settings.py
+++ yektanet/settings.py
@@ -156,4 +156,15 @@ USER_AGENTS_CACHE = 'default'
 
 SESSION_SAVE_EVERY_REQUEST = True
 
-SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
